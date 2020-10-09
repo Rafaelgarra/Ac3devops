dccf784dde25704230c879118dc7d08ef2a4b469
Rafaelgarra <rafaelgaara1@gmail.com>
2020-10-08 21:57:58 -0300

    ac3

---
+++ AcDevops.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def calcula_primos():
+    quantd = 100
+    aculmu = "1, "
+    cont = 0
+    conti = quantd
+    zikin = 0
+    i = 0
+    while zikin <= quantd:
+        cont = 0
+        i += 1
+        for c in range(1, conti):
+            if i % c == 0:
+                cont += 1
+                conti += 1
+        if cont == 2:
+            aculmu = aculmu + str(i) + ", "
+            zikin += 1
+        cont = 0
+    return aculmu
+
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("port", 5000))
+    app.run(host="192.168.15.13", port=port)
